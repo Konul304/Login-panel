d41a258b6565d663a1740a60c41ba6d8db1f77c8
Konul304 <memmedovakonul270@gmail.com>
2023-02-02 12:19:45 +0400

    set values

--- loginpanel/src/components/Register.jsx
+++ loginpanel/src/components/Register.jsx
@@ -1,12 +1,22 @@
 import React, {useState} from "react"
 
 export const Register = (props) => {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
-    const [name, setName] = useState('');
 
+    
     const handleSubmit = (e) =>{
         e.preventDefault();
+        
+        let nameValue = document.getElementsByName('name')[0].value
+        setName(nameValue);
+        let emailValue = document.getElementsByName('email')[0].value
+        setEmail(emailValue);
+        let passValue = document.getElementsByName('password')[0].value
+        setPass(passValue);
+        console.log(name);
+        console.log(pass);
         console.log(email);
     }
 
@@ -20,7 +30,7 @@ export const Register = (props) => {
         <input type="email" placeholder="youremail@gmail.com" name="email"/>
         <label for="password">password</label>
         <input type="password" placeholder="***********" id="password" name="password"/>
-        <button type="submit ">Log In</button>
+        <button type="submit ">Register</button>
        </form>
        <button className="link-btn" onClick={() => {
                 return props.onFormSwitch('login');
